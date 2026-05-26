850f2c3e8e4b81726263f107a9744f69d217323f
elenamartinez520 <elena.martinezet32@gmail.com>
2026-05-26 20:34:19 -0300

    Add files via upload

---
+++ clase17/5/Ejercicio_5.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int fecha;
+            int mes;
+
+            Console.WriteLine("Indicar una fecha como un entero de 6 digitos");
+
+            fecha = Convert.ToInt32(Console.ReadLine());
+
+            mes = (fecha / 100) % 100;
+
+            Console.WriteLine("Fecha: " + fecha);
+            Console.WriteLine("Mes: " + mes);
+
+        }
+    }
+}
---
+++ clase17/6/Ejercicio_6.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float lado;
+            float perimetro;
+            float superficie;
+
+            Console.WriteLine("Ingrese el lado del cuadrado");
+
+            lado = Convert.ToSingle(Console.ReadLine());
+
+            perimetro = lado * 4;
+            superficie = lado * lado;
+
+            Console.WriteLine("Perimetro: " + perimetro);
+            Console.WriteLine("Superficie: " + superficie);
+
+        }
+    }
+}
