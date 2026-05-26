1c90325ca1048eddc07b39e0014cd6e2c36aca71
elenamartinez520 <elena.martinezet32@gmail.com>
2026-05-26 20:29:56 -0300

    Add files via upload

---
+++ clase17/8/Ejercicio_8.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int fahrenheit;
+            int centigrados;
+
+            Console.WriteLine("Ingrese temperatura (Fahrenheit)");
+
+            fahrenheit = Convert.ToInt32(Console.ReadLine());
+
+            centigrados = (fahrenheit - 32) * 5 / 9;
+
+            Console.WriteLine("Temperatura en centigrados: " + centigrados);
+
+        }
+    }
+}
---
+++ clase17/9/Ejercicio_9.cs
@@ -0,0 +1,50 @@
+﻿namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int h1;
+            int m1;
+            int s1;
+
+            int h2;
+            int m2;
+            int s2;
+
+            int tiempo1;
+            int tiempo2;
+            int diferencia;
+
+            Console.WriteLine("Primer instante");
+
+            Console.WriteLine("Hora");
+            h1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Minutos");
+            m1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Segundos");
+            s1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Segundo instante");
+
+            Console.WriteLine("Hora");
+            h2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Minutos");
+            m2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Segundos");
+            s2 = Convert.ToInt32(Console.ReadLine());
+
+            tiempo1 = (h1 * 3600) + (m1 * 60) + s1;
+            tiempo2 = (h2 * 3600) + (m2 * 60) + s2;
+
+            diferencia = tiempo2 - tiempo1;
+
+            Console.WriteLine("Cantidad de segundos: " + diferencia);
+
+        }
+    }
+}
